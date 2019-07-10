4b15cd22969c2bae97ebe1aa973cbcc9fbc7f42c
skadariya <45747122+skadariya@users.noreply.github.com>
2019-07-10 16:09:41 -0700

    Create tree_intersection.m

---
+++ code-challenges/401/ReadMe/tree_intersection.m
@@ -0,0 +1,26 @@
+# Common values in 2 binary trees.
+Implement a find ommon values in 2 binary trees.
+
+
+## Challenge
+
+> Write a function called tree_intersection that takes two binary tree parameters.
+
+> Without utilizing any of the built-in library methods available to your language, return a set of values found in both trees.
+
+## Approach & Efficiency
+> O(n) , constant 
+
+## API
+> tree_intersection : Which accepts the two binary search tree
+
+## Solution
+> ![Solution](https://github.com/skadariya/data-structures-and-algorithms/blob/master/code-challenges/401/assets/ArrayShift.jpg)
+
+## Reference:
+
+
+## Codes
+> [Code Source](https://github.com/skadariya/data-structures-and-algorithms/blob/master/code-challenges/401/src/main/java/codeChallenge/tree_intersection/TreeIntersection.java)
+
+> [Test Code](https://github.com/skadariya/data-structures-and-algorithms/blob/master/code-challenges/401/src/test/java/codeChallenge/tree_intersection/TreeIntersectionTest.java)
